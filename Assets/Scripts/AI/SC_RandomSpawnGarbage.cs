d62adc09fa79b2d87f5c1b0157c14f34ee4729d2
petomuro <55794188+petomuro@users.noreply.github.com>
2020-11-29 12:12:29 +0100

    Merge
    Merge

--- Assets/Scripts/AI/SC_RandomSpawnGarbage.cs
+++ Assets/Scripts/AI/SC_RandomSpawnGarbage.cs
@@ -5,11 +5,7 @@ using UnityEditor;
 
 public class SC_RandomSpawnGarbage : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    public List<GameObject> spawnGameobject;
-=======
     public List<GameObject> SpawnGarbage;
->>>>>>> Stashed changes
     private GameObject AIObject;
     private SC_EnvGrid environmentGrid;
 
@@ -23,12 +19,7 @@ public class SC_RandomSpawnGarbage : MonoBehaviour
     }
 
     void Update() {
-<<<<<<< Updated upstream
-        if(timeToSpawnGameobject >= 500)
-        {
-=======
         if(timeToSpawnGarbage >= 500 && canSpawnGarbage) {
->>>>>>> Stashed changes
             SpawnItemOnFreeCell();
             timeToSpawnGarbage = 0;
         }
@@ -37,23 +28,14 @@ public class SC_RandomSpawnGarbage : MonoBehaviour
     }
 
     private void SpawnItemOnFreeCell() {
-<<<<<<< Updated upstream
-        GameObject randomItem = spawnGameobject[Random.Range(0, (spawnGameobject.Count - 1))];
-=======
         GameObject randomItem = SpawnGarbage[Random.Range(0, (SpawnGarbage.Count - 1))];
->>>>>>> Stashed changes
         Vector3 AIPos = AIObject.transform.position;
         Vector2 AIPosToGrid = environmentGrid.worldToGrid(AIPos);
         GridCell firstFreeCell = environmentGrid.findFirstFreeCell(AIPosToGrid);
         Vector3 firstFreeCellWorldPos = environmentGrid.gridToWorld(firstFreeCell.getPosition());
     
-<<<<<<< Updated upstream
-        Vector3 spawnGameobjectPosition = new Vector3(firstFreeCellWorldPos.x, AIPos.y + .5f, firstFreeCellWorldPos.z);
-        Instantiate(randomItem, spawnGameobjectPosition, Quaternion.identity);
-=======
         Vector3 SpawnGarbagePosition = new Vector3(firstFreeCellWorldPos.x, AIPos.y + .5f, firstFreeCellWorldPos.z);
         Instantiate(randomItem, SpawnGarbagePosition, Quaternion.identity);
->>>>>>> Stashed changes
         environmentGrid.cellAddObject(randomItem);
     }
 }
